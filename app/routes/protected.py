be8fca7cd974fe08ff807b277c7e6c0120bd8787
lucasxaviervieira <lucasdavid.xavier@gmail.com>
2025-04-03 06:32:27 -0300

    remove: delete useless route

--- app/routes/protected.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify, request
-from flask_jwt_extended import jwt_required
-
-protected_bp = Blueprint('protected', __name__)
-
-@protected_bp.route('/test-protected', methods=['GET'])
-@jwt_required()
-def test_protected():
-    return jsonify({"message": "This is a protected route!"}), 200
